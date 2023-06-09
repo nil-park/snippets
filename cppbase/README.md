4cdd967d68f2163b44550a5ca99784701edaeee4
nil-park <kwang0621@gmail.com>
2023-06-09 12:12:36 +0900

    Modified the template to use test cases read from files.

---
+++ cppbase/README.md
@@ -0,0 +1,34 @@
+# Basic test environment for coding test with cpp
+
+# MacOS
+```
+g++ --version
+```
+Then the example output of the above command is like below.
+```
+Apple clang version 14.0.0 (clang-1400.0.29.102)
+Target: x86_64-apple-darwin22.1.0
+Thread model: posix
+InstalledDir: /Library/Developer/CommandLineTools/usr/bin
+```
+Create the bits dir at the path like below, and then edit stdc++.h
+```
+sudo mkdir -p /Library/Developer/CommandLineTools/usr/include/bits
+sudo vi /Library/Developer/CommandLineTools/usr/include/bits/stdc++.h
+```
+Then copy and paste https://gist.github.com/frankchen0130/9ac562b55fa7e03689bca30d0e52b0e5
+
+# Run Test
+```
+g++ -g -std=c++20 main.cpp && ./a.out testcases/input00.txt testcases/output00.txt
+```
+
+# Environment for each problem solving
+```
+git checkout -b "solve/lc/count-of-integers"
+```
+After solving the problem, then delete the branch.
+```
+git checkout main
+git branch -D "solve/lc/count-of-integers"
+```
--- cppbase/main.cpp
+++ cppbase/main.cpp
@@ -6,10 +6,6 @@
 
 using namespace std;
 
-string ltrim(const string &);
-string rtrim(const string &);
-vector<string> split(const string &);
-
 /*
  * Complete the 'maximumToys' function below.
  *
@@ -20,79 +16,76 @@ vector<string> split(const string &);
  */
 
 int maximumToys(vector<int> prices, int k) {
-
-}
-
-int main()
-{
-    ofstream fout(getenv("OUTPUT_PATH"));
-
-    string first_multiple_input_temp;
-    getline(cin, first_multiple_input_temp);
-
-    vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
-
-    int n = stoi(first_multiple_input[0]);
-
-    int k = stoi(first_multiple_input[1]);
-
-    string prices_temp_temp;
-    getline(cin, prices_temp_temp);
-
-    vector<string> prices_temp = split(rtrim(prices_temp_temp));
-
-    vector<int> prices(n);
-
-    for (int i = 0; i < n; i++) {
-        int prices_item = stoi(prices_temp[i]);
-
-        prices[i] = prices_item;
-    }
-
-    int result = maximumToys(prices, k);
-
-    fout << result << "\n";
-
-    fout.close();
-
     return 0;
 }
 
-string ltrim(const string &str) {
-    string s(str);
-
-    s.erase(
-        s.begin(),
-        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
-    );
-
-    return s;
-}
-
-string rtrim(const string &str) {
-    string s(str);
-
-    s.erase(
-        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
-        s.end()
-    );
-
-    return s;
-}
-
-vector<string> split(const string &str) {
-    vector<string> tokens;
-
-    string::size_type start = 0;
-    string::size_type end = 0;
-
-    while ((end = str.find(" ", start)) != string::npos) {
-        tokens.push_back(str.substr(start, end - start));
-
-        start = end + 1;
+int main(int argc, char* argv[])
+{
+    if (argc != 3) {
+        fprintf(stderr, "Please input test case file paths.\nExample: ./a.out testcases/input00.txt testcases/output00.txt\n");
+        return 1;
     }
 
-    tokens.push_back(str.substr(start));
+    // Open the input test case file.
+    FILE * fp = fopen(argv[1], "r");
+    if (fp == NULL) {
+        fprintf(stderr, "Input file not found at %s", argv[1]);
+        return 2;
+    }
+    
+    printf("\n\033[96mInputs:\033[0m\n\n");
 
-    return tokens;
+    // read first line of code
+    int n, k;
+    fscanf(fp, "%d %d", &n, &k);
+    printf("Size of array (n) is \033[93m%d\033[0m\n", n);
+    printf("Mark's budget (k) is \033[93m%d\033[0m\n", k);
+
+    // read array elements
+    printf("\nPrices: \033[93m");
+    vector<int> prices(n);
+    for (int i = 0; i < n; i++) {
+        fscanf(fp, "%d", &prices[i]);
+        printf("%d ", prices[i]);
+    }
+    printf("\033[0m\n");
+
+    /* Read all the line of codes.
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t readBytes;
+    while ((readBytes = getline(&line, &len, fp)) != -1) {
+        printf("Retrieved line of length %zu:\n", readBytes);
+        printf("%s\n", line);
+    }
+    if (line)
+        free(line);
+    */
+
+    fclose(fp);
+
+    // Open the output test case file.
+    fp = fopen(argv[1], "r");
+    if (fp == NULL) {
+        fprintf(stderr, "Output file not found at %s", argv[1]);
+        return 2;
+    }
+
+    // read first line of code
+    int gt;
+    fscanf(fp, "%d", &gt);
+    fclose(fp);
+
+    // run the function
+    int out = maximumToys(prices, k);
+
+    printf("\n\033[96mOutput:\033[0m\n\n");
+    if (out == gt) printf("\033[92m"); else printf("\033[91m");
+    printf("%d", out);
+    printf("\033[0m\n");
+
+    printf("\n\033[96mExpected:\033[0m\n\n");
+    printf("\033[93m%d\033[0m\n\n", gt);
+
+    return 0;
 }
